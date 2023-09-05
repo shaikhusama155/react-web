4e14ac9d1b1bc25b67f9667d6a3e91060f8107a6
shaikhusama155 <shaikhusama155@gmail.com>
2023-09-05 11:13:36 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import Slider from './component/Slider';
+import  Image  from './component/img/1.jpg';
+import  Image1  from './component/img/2.avif';
+import  Image2  from './component/img/3.avif';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbar/>
+     <Slider/>
     </div>
   );
 }
---
+++ src/component/Slider.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Slider() {
+  return (
+    <div>
+<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="./img/1.jpg" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="./img/2.avif" className="d-block w-100" alt="..."/>
+    </div>
+    <div className="carousel-item">
+      <img src="./img/3.avif" className="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+  )
+}
